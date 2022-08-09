e5c207338406f8f7ff1b6e88d0780a6554034770
EricTGoto <erictomokigoto@gmail.com>
2022-08-09 14:50:23 -0700

    wrote GET /api/robots

--- controllers/robots.js
+++ controllers/robots.js
@@ -1,4 +1,12 @@
 const robotsAPIRouter = require('express').Router();
 const Robot = require('../models/robot');
 
+robotsAPIRouter.get('/', (request, response, next) => {
+  Robot.find({})
+    .then((robots) => {
+      response.status(200).json(robots);
+    })
+    .catch((error) => next(error));
+});
+
 module.exports = robotsAPIRouter;
